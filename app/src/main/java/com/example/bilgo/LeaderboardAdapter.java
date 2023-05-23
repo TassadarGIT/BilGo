fb1ace621cefd4878153f32b761c1eab7db9ec65
Emre Yazicioglu <emreyaziciogl@gmail.com>
2023-05-23 15:37:34 +0300

    Pushing untracked files including LeaderboardFragment and LeaderboardAdapter

---
+++ app/src/main/java/com/example/bilgo/LeaderboardAdapter.java
@@ -0,0 +1,60 @@
+package com.example.bilgo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bilgo.model.UserModel;
+
+import java.util.List;
+
+public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.ViewHolder> {
+    private List<UserModel> userList;
+
+    public LeaderboardAdapter(List<UserModel> userList) {
+        this.userList = userList;
+    }
+
+    public void updateData(List<UserModel> updatedUserList) {
+        userList = updatedUserList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_view, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        UserModel user = userList.get(position);
+        holder.bind(user);
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTextView;
+        private TextView pointsTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.name);
+            pointsTextView = itemView.findViewById(R.id.points);
+        }
+
+        public void bind(UserModel user) {
+            nameTextView.setText(user.getName() + " " + user.getSurname());
+            pointsTextView.setText(String.valueOf(user.getPoints()));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bilgo/LeaderboardFragment.java
@@ -0,0 +1,60 @@
+package com.example.bilgo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bilgo.model.UserModel;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+
+import java.util.List;
+
+public class LeaderboardFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private LeaderboardAdapter leaderboardAdapter;
+    private FirebaseFirestore firestore;
+    private CollectionReference leaderboardRef;
+    private List<UserModel> userList;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        firestore = FirebaseFirestore.getInstance();
+        leaderboardRef = firestore.collection("users");
+        leaderboardAdapter = new LeaderboardAdapter(userList);
+
+        leaderboardRef.orderBy("points", Query.Direction.DESCENDING).addSnapshotListener((querySnapshot, error) -> {
+            if (error != null) {
+                // TODO: Handle the error
+                return;
+            }
+
+            if (querySnapshot != null) {
+                userList = querySnapshot.toObjects(UserModel.class);
+                leaderboardAdapter.updateData(userList);
+                recyclerView.setAdapter(leaderboardAdapter);
+            }
+        });
+    }
+}
