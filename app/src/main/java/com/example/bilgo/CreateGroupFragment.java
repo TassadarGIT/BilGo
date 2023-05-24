04dbccbab256179eb4b86ce4c3057c4a4915c748
İlke Latifoğlu <ilkeltfgl@gmail.com>
2023-05-24 17:31:53 +0300

    user interface of creating groups for hitchhiking and taxi sharing-v1.1

---
+++ app/src/main/java/com/example/bilgo/CreateGroupFragment.java
@@ -0,0 +1,26 @@
+package com.example.bilgo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CreateGroupFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_group, container, false);
+    }
+}
