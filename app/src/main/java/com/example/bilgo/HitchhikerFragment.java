d8bda29ac91a30cb221ad2ce7614b86a988c89f5
Elif <lara.oguzhan@ug.bilkent.edu.tr>
2023-05-25 12:28:38 +0300

    Button functions added to hitchhiking option.

---
+++ app/src/main/java/com/example/bilgo/HitchhikerFragment.java
@@ -0,0 +1,65 @@
+package com.example.bilgo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bilgo.model.TripModel;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.List;
+
+public class HitchhikerFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private TaxiAdapter tripAdapter;
+    private FirebaseFirestore firestore;
+    private CollectionReference tripRef;
+    private List<TripModel> tripList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hitchhiker, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        firestore = FirebaseFirestore.getInstance();
+        tripRef = firestore.collection("trips");
+        tripAdapter = new TaxiAdapter(tripList);
+
+        tripRef.orderBy("seatsAvailable", Query.Direction.DESCENDING).addSnapshotListener((querySnapshot, error) -> {
+            if (error != null) {
+                // TODO: Handle the error
+                return;
+            }
+
+            if (querySnapshot != null) {
+                tripList = querySnapshot.toObjects(TripModel.class);
+                tripAdapter.updateData(tripList);
+                recyclerView.setAdapter(tripAdapter);
+            }
+        });
+    }
+}
