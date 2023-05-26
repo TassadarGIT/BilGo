8cbfeb34018ec390326f56bd1047d2f82c140d3e
İlke Latifoğlu <ilkeltfgl@gmail.com>
2023-05-26 21:59:39 +0300

    final version of spinners to choose a time slot for the trip

--- app/src/main/java/com/example/bilgo/CreateGroupFragment.java
+++ app/src/main/java/com/example/bilgo/CreateGroupFragment.java
@@ -1,30 +1,86 @@
 package com.example.bilgo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class CreateGroupFragment extends Fragment {
 
     private Button createBtn;
+    private String hour;
+    private String minute;
+
+    private Spinner hourSpinner;
+    private Spinner minuteSpinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
+    //hour selection
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create_group, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_create_group, container, false);
+        hourSpinner = rootView.findViewById(R.id.spinnerHours);
+        minuteSpinner = rootView.findViewById(R.id.spinnerMinutes);
+        inItSpinnerHours();
+        inItSpinnerMinutes();
+        return rootView;
+    }
+
+    private void inItSpinnerHours() {
+        String[] items = new String[]{
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, items);
+        hourSpinner.setAdapter(adapter);
+        hourSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+    private void inItSpinnerMinutes() {
+        String[] items = new String[]{
+                "00", "15", "30", "45"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, items);
+        minuteSpinner.setAdapter(adapter);
+        minuteSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -32,5 +88,4 @@ public class CreateGroupFragment extends Fragment {
 
         createBtn = view.findViewById(R.id.createBtn);
 
-    }
-}
+    }}
